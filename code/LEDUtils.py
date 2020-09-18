c852616287822a31c36ecd72aeb3b80ad26977f7
Foxdudes <67207567+Foxdudes@users.noreply.github.com>
2020-09-18 12:49:02 +0200

    Update LEDUtils.py

--- code/LEDUtils.py
+++ code/LEDUtils.py
@@ -248,7 +248,7 @@ class PingPongBoard:
 	# Rainbow color animation
 	def rainbow(self,wait_ms=20):
 		# Draw rainbow that fades across all pixels at once.
-		j = self.updateFrame(self.led_count)
+		j = self.updateFrame((self.led_count+self.led_count))
 
 		for x in range(self.num_cols):
 			for y in range(self.num_rows):
@@ -267,7 +267,7 @@ class PingPongBoard:
 			for y in range(self.num_rows):
 				i = x*self.num_rows + y
 				if self.balls[y][x].text == True:
-					self.writeBallColor(x,y,self.wheel(((i*PIXEL_RATIO)+j) & 255))
+					self.writeBallColor(x,y,self.wheel(((i*PIXEL_RATIO*2)+j) & 255))
 		self.strip.show()
 		time.sleep(wait_ms/1000.0)
 
